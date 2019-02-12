38cbc96300669411b5d1d8c5cd87ecba508a4b29
Wangweizhen <wangweizhen@bilibili.com>
2019-02-12 17:08:31 +0800

    u

--- libwebp-sys/examples/cwebp.rs
+++ libwebp-sys/examples/cwebp.rs
@@ -9,21 +9,22 @@ use lodepng;
 fn main() {
     let path = Path::new("in.png");
     let mut state = lodepng::State::new();
-
-    match state.decode_file(&path) {
-        Ok(image) => match image {
-            lodepng::Image::RGBA(bitmap) => {
-                println!("Decoded image {} x {}", bitmap.width, bitmap.height);
-                println!("The first pixel is {}", bitmap.buffer[0]);
-                println!("The raw bytes are {:?}", bitmap.buffer.as_bytes());
-            }
-            x => println!("Decoded some other image format {:?}", x),
-        },
-        Err(reason) => println!("Could not load {}, because: {}", path.display(), reason),
-    }
     let wp: *mut libwebp_sys::WebPPicture = &mut Default::default();
     unsafe {
         libwebp_sys::WebPPictureAlloc(wp);
+        match state.decode_file(&path) {
+            Ok(image) => match image {
+                lodepng::Image::RGBA(bitmap) => {
+                    wp.height = bitmap.height;
+                    wp.width = bitmap.width;
+                    println!("Decoded image {} x {}", bitmap.width, bitmap.height);
+                    println!("The first pixel is {}", bitmap.buffer[0]);
+                    println!("The raw bytes are {:?}", bitmap.buffer.as_bytes());
+                }
+                x => println!("Decoded some other image format {:?}", x),
+            },
+            Err(reason) => println!("Could not load {}, because: {}", path.display(), reason),
+        }
 
         libwebp_sys::WebPPictureFree(wp);
     }
