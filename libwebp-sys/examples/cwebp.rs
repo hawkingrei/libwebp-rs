b2680208bed9d201aa04a5b48097085c8d07965d
Wangweizhen <wangweizhen@bilibili.com>
2019-02-12 15:53:28 +0800

    u

--- libwebp-sys/examples/cwebp.rs
+++ libwebp-sys/examples/cwebp.rs
@@ -4,7 +4,7 @@ use std::default::Default;
 
 fn main() {
     let wp: *mut libwebp_sys::WebPPicture = &mut Default::default();
-    libwebp_sys::WebPPictureAlloc(libwebp_sys);
+    libwebp_sys::WebPPictureAlloc(wp);
 
-    libwebp_sys::WebPPictureFree(libwebp_sys);
+    libwebp_sys::WebPPictureFree(wp);
 }
