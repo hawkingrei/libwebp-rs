2757ae4e939234f1328fd5b45b0eb47877b0ff15
Wangweizhen <wangweizhen@bilibili.com>
2019-02-12 16:56:56 +0800

    u

--- libwebp-sys/examples/cwebp.rs
+++ libwebp-sys/examples/cwebp.rs
@@ -1,12 +1,30 @@
 extern crate libwebp_sys;
 
+use rgb::*;
 use std::default::Default;
+use std::path::Path;
+
+use lodepng;
 
 fn main() {
+    let path = Path::new("in.png");
+    let mut state = lodepng::State::new();
+
+    match state.decode_file(&path) {
+        Ok(image) => match image {
+            lodepng::Image::RGBA(bitmap) => {
+                println!("Decoded image {} x {}", bitmap.width, bitmap.height);
+                println!("The first pixel is {}", bitmap.buffer[0]);
+                println!("The raw bytes are {:?}", bitmap.buffer.as_bytes());
+            }
+            x => println!("Decoded some other image format {:?}", x),
+        },
+        Err(reason) => println!("Could not load {}, because: {}", path.display(), reason),
+    }
     let wp: *mut libwebp_sys::WebPPicture = &mut Default::default();
     unsafe {
-    libwebp_sys::WebPPictureAlloc(wp);
+        libwebp_sys::WebPPictureAlloc(wp);
 
-    libwebp_sys::WebPPictureFree(wp);
+        libwebp_sys::WebPPictureFree(wp);
     }
 }
