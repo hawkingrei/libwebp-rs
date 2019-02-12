43db6c69a7204bd263da1a12a063433954c675ea
Wangweizhen <wangweizhen@bilibili.com>
2019-02-12 17:29:31 +0800

    u

--- libwebp-sys/examples/cwebp.rs
+++ libwebp-sys/examples/cwebp.rs
@@ -1,10 +1,11 @@
 extern crate libwebp_sys;
 
-use rgb::*;
 use std::default::Default;
+use std::mem;
 use std::path::Path;
 
 use lodepng;
+use rgb::*;
 
 fn main() {
     let path = Path::new("in.png");
@@ -17,9 +18,15 @@ fn main() {
                 lodepng::Image::RGBA(bitmap) => {
                     (*wp).height = bitmap.height as i32;
                     (*wp).width = bitmap.width as i32;
+                    let stride = 4 * bitmap.width * mem::size_of::<u8>();
                     println!("Decoded image {} x {}", bitmap.width, bitmap.height);
+                    libwebp_sys::WebPPictureImportRGBA(
+                        wp,
+                        bitmap.buffer.as_bytes().as_ptr(),
+                        stride as i32,
+                    );
                     println!("The first pixel is {}", bitmap.buffer[0]);
-                    println!("The raw bytes are {:?}", bitmap.buffer.as_bytes());
+                    //println!("The raw bytes are {:?}", bitmap.buffer.as_bytes());
                 }
                 x => println!("Decoded some other image format {:?}", x),
             },
