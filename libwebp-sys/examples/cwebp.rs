628bd07c0014d800514b638549dfcbbd21aba916
Wangweizhen <wangweizhen@bilibili.com>
2019-02-12 15:49:53 +0800

    u

--- libwebp-sys/examples/cwebp.rs
+++ libwebp-sys/examples/cwebp.rs
@@ -1,8 +1,10 @@
 extern crate libwebp_sys;
 
-
 use std::default::Default;
 
 fn main() {
     let wp: *mut libwebp_sys::WebPPicture = &mut Default::default();
+    libwebp_sys::WebPPictureAlloc(libwebp_sys);
+
+    libwebp_sys::WebPPictureFree(libwebp_sys);
 }
