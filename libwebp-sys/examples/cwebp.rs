130e1e97fff1abf865d3dabdef402f2a381e4859
Wangweizhen <wangweizhen@bilibili.com>
2019-02-13 00:04:03 +0800

    u

--- libwebp-sys/examples/cwebp.rs
+++ libwebp-sys/examples/cwebp.rs
@@ -1,11 +1,26 @@
 extern crate libwebp_sys;
 
 use std::default::Default;
+use std::ffi::CStr;
 use std::mem;
 use std::path::Path;
 
+use libc;
 use lodepng;
 use rgb::*;
+
+unsafe extern "C" fn MyViewer(
+    data: *const u8,
+    data_size: usize,
+    picture: *const libwebp_sys::WebPPicture,
+) -> libc::c_int {
+    let out: *mut libc::FILE = (*picture).custom_ptr as *mut libc::FILE;
+    if data_size > 0 {
+        libc::fwrite(data as *const libc::c_void, data_size, 1, out) as i32
+    } else {
+        1
+    }
+}
 
 #[inline(always)]
 unsafe fn WebPConfigInit(config: *mut libwebp_sys::WebPConfig) -> libc::c_int {
@@ -22,8 +37,16 @@ fn main() {
     let mut state = lodepng::State::new();
     let wp: *mut libwebp_sys::WebPPicture = &mut Default::default();
     let config: *mut libwebp_sys::WebPConfig = &mut Default::default();
+
     unsafe {
+        let c_file = libc::fopen(
+            CStr::from_bytes_with_nul_unchecked(b"out.webp\0").as_ptr(),
+            CStr::from_bytes_with_nul_unchecked(b"wb\0").as_ptr(),
+        );
         libwebp_sys::WebPPictureAlloc(wp);
+
+        (*wp).writer = Some(MyViewer);
+        (*wp).custom_ptr = c_file as *mut libc::c_void;
         WebPConfigInit(config);
         match state.decode_file(&path) {
             Ok(image) => match image {
@@ -46,7 +69,7 @@ fn main() {
             },
             Err(reason) => println!("Could not load {}, because: {}", path.display(), reason),
         }
-
+        libc::fclose(c_file);
         libwebp_sys::WebPPictureFree(wp);
     }
 }
