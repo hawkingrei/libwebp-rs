023126cb15cc755b73843c4c8c365b23c31fdbc7
Wangweizhen <wangweizhen@bilibili.com>
2019-02-13 17:53:07 +0800

    u

--- libwebp-sys/src/lib.rs
+++ libwebp-sys/src/lib.rs
@@ -36,33 +36,35 @@ unsafe fn WebPConfigInit(config: *mut WebPConfig) -> libc::c_int {
 
 #[bench]
 fn bench_png_to_webp(b: &mut Bencher) {
+    // 1 (inclusive) to 21 (exclusive)
     b.iter(|| {
         let path = Path::new("in.png");
         let mut state = lodepng::State::new();
         let wp: *mut WebPPicture = &mut Default::default();
         let config: *mut WebPConfig = &mut Default::default();
-
         unsafe {
             WebPPictureAlloc(wp);
 
             WebPConfigInit(config);
+
             match state.decode_file(&path) {
                 Ok(image) => match image {
                     lodepng::Image::RGBA(bitmap) => {
                         (*wp).height = bitmap.height as i32;
                         (*wp).width = bitmap.width as i32;
                         let stride = 4 * bitmap.width * mem::size_of::<u8>();
-                        println!("Decoded image {} x {}", bitmap.width, bitmap.height);
+
                         WebPPictureImportRGBA(wp, bitmap.buffer.as_bytes().as_ptr(), stride as i32);
 
-                        println!("The first pixel is {}", bitmap.buffer[0]);
                         WebPEncode(config, wp);
+
                         //println!("The raw bytes are {:?}", bitmap.buffer.as_bytes());
                     }
                     x => println!("Decoded some other image format {:?}", x),
                 },
                 Err(reason) => println!("Could not load {}, because: {}", path.display(), reason),
             }
+
             WebPPictureFree(wp);
         }
     });
