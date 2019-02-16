a51acfa4bbfca0600df3e2f4cb170390589e7765
Weizhen Wang <hawking.rei@gmail.com>
2019-02-16 12:48:32 +0800

    support jpeg (#5)

---
+++ libjpeg-turbo-sys/build.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::path::Path;
+use std::path::PathBuf;
+
+static TURBO_INCLUDE_DIR: &'static str = "/opt/libjpeg-turbo";
+
+fn main() {
+    println!("cargo:rustc-link-lib=libjpeg");
+    // hzy: Don't use SPDK_DIR as environment variable here as SPDK 18.07 rely on this variable to
+    // build (i.e. will fail the SPDK build if we use the same environment variable here)
+
+    let turbo_include_path = env::var("TURBO_INCLUDE").unwrap_or(TURBO_INCLUDE_DIR.to_string());
+
+    let include_path_jpeg_dir = format!("-I{}/include", turbo_include_path);
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        .clang_arg(include_path_jpeg_dir)
+        .derive_default(true)
+        // The input header we would like to generate
+        // bindings for.
+        .header("wrapper.h")
+        .blacklist_type("IPPORT_.*") // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
+        .blacklist_type("max_align_t") // https://github.com/rust-lang-nursery/rust-bindgen/issues/550
+        .rustfmt_bindings(true)
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = Path::new("./src/");
+    bindings
+        .write_to_file(out_path.join("jpeg_bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    println!("cargo:rerun-if-changed=./build.rs");
+    println!("cargo:rerun-if-changed=src/wrapper.h");
+    println!("cargo:rustc-link-search=native=/opt/libjpeg-turbo/lib64");
+}
---
+++ libjpeg-turbo-sys/wrapper.h
@@ -0,0 +1,7 @@
+#include "stdlib.h"
+#include <stdio.h>
+
+#include "jerror.h"
+#include "jconfig.h"
+#include "jmorecfg.h"
+#include "jpeglib.h"
